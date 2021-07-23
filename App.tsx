389e5eea210fb7472f0db47ee259ef89e6bbaf5e
CRS <kristian192019d@gmail.com>
2021-07-22 21:21:54 -0500

    Added Log In screen

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import {Image, ScrollView, StatusBar, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {TopBar} from './src/components/TopBar';
 import {StationScreen} from './src/screens/StationScreen';
+import {LogIn} from './src/screens/LogIn/LogIn';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar hidden={true} />
-      <TopBar title="estación experimental tunshi" />
-      <StationScreen />
+      {/**<TopBar title="estación experimental tunshi" />**/}
+      {/**<StationScreen />**/}
+      <LogIn />
     </NavigationContainer>
   );
 };
--- src/theme/GlobalStyles.tsx
+++ src/theme/GlobalStyles.tsx
@@ -55,4 +55,65 @@ export const styles = StyleSheet.create({
     paddingVertical: 19,
     paddingLeft: 64,
   },
+  ImageCardLoginFooter: {
+    position: 'absolute',
+    backgroundColor: 'black',
+    width: 311,
+    height: 94,
+    opacity: 0.38,
+  },
+  ImagenCardContainer: {
+    width: 311,
+    height: 374,
+    marginRight: 11,
+    justifyContent: 'flex-end',
+  },
+  LogInContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  LogInHeader: {
+    flex: 1,
+  },
+  LogInBodyContainer: {
+    height: 325,
+    flexDirection: 'row',
+  },
+  LogInTitle: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontSize: 48,
+    textAlign: 'center',
+    letterSpacing: 0.15,
+  },
+  LogInTitleContainer: {
+    justifyContent: 'center',
+    marginLeft: 313,
+  },
+  LogInFormContainer: {
+    justifyContent: 'flex-start',
+    marginLeft: 40,
+  },
+  LogInFooter: {
+    height: 55,
+    backgroundColor: '#C22321',
+  },
+  BorderButtom: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 5,
+    borderWidth: 2,
+    height: 36,
+    borderColor: 'green',
+  },
+  BorderButtomTitle: {
+    textTransform: 'uppercase',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 14,
+    paddingHorizontal: 15,
+    letterSpacing: 1.25,
+  },
 });
